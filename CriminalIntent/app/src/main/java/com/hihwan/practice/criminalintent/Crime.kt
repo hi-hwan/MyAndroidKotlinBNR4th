9e70532c9ead84894ac9b21a5cdbe044251abb55
Vincent Hwang <vincent.hwang@pointmobile.com>
2022-01-21 19:18:21 +0900

    [CriminalIntent] Chapter 10 (Finish), Page 244
     - Challenge 추가
       * 날짜 형식 변경 (DateFormat)

--- CriminalIntent/app/src/main/java/com/hihwan/practice/criminalintent/Crime.kt
+++ CriminalIntent/app/src/main/java/com/hihwan/practice/criminalintent/Crime.kt
@@ -1,13 +1,12 @@
 package com.hihwan.practice.criminalintent
 
+import android.text.format.DateFormat
 import java.util.*
 
 data class Crime(
     val id: UUID = UUID.randomUUID(),
     var title: String = "",
-    var date: Date = Date(),
+    var date: CharSequence = DateFormat.format("EEEE, MMM d, yyyy", Date()),
     var isSolved: Boolean = false,
     var requiresPolice: Boolean = false
 )
-
-
